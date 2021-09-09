1efffd21387bfecc5240d3e6a880117a6f334758
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-09-09 03:57:20 +0300

    Setting maven

---
+++ src/main/java/com/dima/graphics/GraphicsAppRunner.java
@@ -0,0 +1,13 @@
+package com.dima.graphics;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GraphicsAppRunner {
+
+    public static void main(String[] args) {
+        log.info("RUN BEGIN");
+        System.out.println("RUN!!!!");
+        log.info("RUN END");
+    }
+}
