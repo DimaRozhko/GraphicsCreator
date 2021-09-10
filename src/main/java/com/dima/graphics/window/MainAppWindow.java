53452ffbcbc2f4edab55e9605a736daf7fe2129c
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-09-10 23:42:42 +0300

    Simple geometrical figures and panele

--- src/main/java/com/dima/graphics/window/MainAppWindow.java
+++ src/main/java/com/dima/graphics/window/MainAppWindow.java
@@ -1,21 +1,19 @@
 package com.dima.graphics.window;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static com.dima.graphics.constants.Constants.HEIGHT_OF_WINDOW_PIXELS;
 import static com.dima.graphics.constants.Constants.WIDTH_OF_WINDOW_PIXELS;
@@ -34,25 +32,34 @@ public class MainAppWindow extends Application {
         primaryStage.setHeight(HEIGHT_OF_WINDOW_PIXELS);
 
         GridPane graphicsPanel = new GridPane();
-
-        graphicsPanel.getColumnConstraints().addAll(new ColumnConstraints(10),
-                new ColumnConstraints(110),
-                new ColumnConstraints(50));
-
-        List<Integer> integers = IntStream.range(0, 100).boxed().collect(Collectors.toList());
-        log.info("LIST BEGIN");
-        integers.stream().forEach(System.out::println);
-        log.info("LIST END");
-//        graphicsPanel.getColumnConstraints().addAll(IntStream.range(0, 12)
-//                .collect(Collectors.toList()));
+//
+        List<Integer> integers = IntStream.range(0, 20).boxed().collect(Collectors.toList());
+//        log.info("LIST BEGIN");
+//        integers.stream().forEach(System.out::println);
+//        log.info("LIST END");
+        List<ColumnConstraints> columnConstraintsList = integers.stream()
+                .map(number -> new ColumnConstraints(5)).collect(Collectors.toList());
+        graphicsPanel.getColumnConstraints().addAll(columnConstraintsList);
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(5);
+        rectangle.setHeight(5);
+        rectangle.setFill(Color.RED);
+        GridPane.setRowIndex(rectangle, 0);
+        GridPane.setColumnIndex(rectangle, 2);
+        graphicsPanel.getChildren().add(rectangle);
+        graphicsPanel.getRowConstraints().add(new RowConstraints(5));
+        graphicsPanel.setBackground(new Background(new BackgroundFill(Color.AQUA, CornerRadii.EMPTY, Insets.EMPTY)));
         graphicsPanel.setGridLinesVisible(true);
-        graphicsPanel.getChildren().addAll(new Label(), new Label(), new Label());
+
+
+
+        StackPane graphicsStackPane = new StackPane(graphicsPanel);
 
         Label helloWorldLabel = new Label("Hello world!");
         Button buttonClick = new Button("Click me");
         FlowPane flowPane = new FlowPane();
 
-        flowPane.getChildren().addAll(graphicsPanel, helloWorldLabel, buttonClick);
+        flowPane.getChildren().addAll(helloWorldLabel, graphicsStackPane, buttonClick);
         Scene primaryScene = new Scene(flowPane);
         primaryStage.setScene(primaryScene);
 
